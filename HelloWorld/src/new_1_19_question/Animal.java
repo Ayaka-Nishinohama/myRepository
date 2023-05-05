3339fcdd5e777c85977dba16d28d7426f3c98cfc
Ayaka Nishinohama <r.ike0810@gmail.com>
2023-05-05 09:44:27 +0900

    [1.19 クラスの基本]

--- HelloWorld/src/new_1_19_question/Animal.java
+++ HelloWorld/src/new_1_19_question/Animal.java
@@ -1,15 +1,30 @@
 package new_1_19_question;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Animal {
 
 	public static void main(String[] args) {
 		
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		
+		// Dogクラスに動物の名前,数を代入
+		Dog dog= new Dog("猫",200);
+		
+		// 動物の名前をコンソールに表示
+		System.out.println(dog.animal);
+		
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		
+		// 動物の数をコンソールに表示
+		System.out.println(dog.number);
 		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		
+	    Date date = new Date();
+	    SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd E HH:mm:ss");
+	    System.out.println(df.format(date));
 	}
-
+	
 }
--- HelloWorld/src/new_1_19_question/Dog.java
+++ HelloWorld/src/new_1_19_question/Dog.java
@@ -4,24 +4,28 @@ public class Dog {
 
 	// Q1：フィールドに動物の名前の変数を定義してください。
 	
+	// 変数animalを定義
 	public String animal;
 	
 	// Q2：フィールドに動物の数の変数を定義してください。
 	
+	// 動物の数を指定する変数を定義
 	public int number;
 	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	
-	public Dog(){
+	// 引数を2つ渡すコンストラクタを作成
+	public Dog(String animal,int number){
+		
+		// 変数animalに犬を代入
+		this.animal = animal;
 		animal = "犬";
-	}
 			
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
 	
-	Dog(int number){
-		this.number = ;
+		// 指定した引数を変数numberに代入する
+		this.number = number;
+	
 	}
 	
-}
-
 }
