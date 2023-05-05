77e64cd71b873225fa9c409c2ba342a2906a4cd2
Ayaka Nishinohama <r.ike0810@gmail.com>
2023-05-05 09:48:30 +0900

    [1.19 クラスの基本]

--- HelloWorld/src/new_1_19_question/Animal.java
+++ HelloWorld/src/new_1_19_question/Animal.java
@@ -6,34 +6,34 @@ import java.util.Date;
 public class Animal {
 
 	public static void main(String[] args) {
-		
+
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		
+
 		// Dogクラスに動物の名前,数を代入
 		Dog dog= new Dog("猫",200);
-		
+
 		// 動物の名前をコンソールに表示
 		System.out.println(dog.animal);
-		
+
 		//　------------------------------------------------------------------------
-		
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		
+
 		// 動物の数をコンソールに表示
 		System.out.println(dog.number);
-		
+
 		//　------------------------------------------------------------------------
-		
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		
+
 		// 日時を取得
-	    Date date = new Date();
-	    
-	    // 取得した日時をフォーマット
-	    SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd E HH:mm:ss");
-	    
-	    // フォーマットした日時をコンソールに表示
-	    System.out.println(df.format(date));
+		Date date = new Date();
+
+		// 取得した日時をフォーマット
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd E HH:mm:ss");
+
+		// フォーマットした日時をコンソールに表示
+		System.out.println(df.format(date));
 	}
-	
+
 }
