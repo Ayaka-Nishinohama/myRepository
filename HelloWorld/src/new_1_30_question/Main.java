d7961b6ebea0de697eecf31e5b15a5bb02f73718
Ayaka Nishinohama <r.ike0810@gmail.com>
2023-06-15 23:19:14 +0900

    [1.30 自己紹介プログラムを作成①]

---
+++ HelloWorld/src/new_1_30_question/Main.java
@@ -0,0 +1,10 @@
+package new_1_30_question;
+
+class Main {
+	public static void main(String[] args) {
+		//インスタンスを作成
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60.4);
+		//printメソッドを呼び出す
+		person1.print();
+	}
+}
